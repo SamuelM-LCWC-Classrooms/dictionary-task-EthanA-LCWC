c28376f08f7e78cdbebe791043d96723685c43bf
EthanA-LCWC <144792227+EthanA-LCWC@users.noreply.github.com>
2024-09-16 12:44:33 +0000

    I made the code that calculates the needs, wants and savings

--- task.py
+++ task.py
@@ -3,5 +3,9 @@ def task(input): # the value of input is an integer which is the total money ava
     result = {} # Empty dictionary
 
     # Code here...
+    needs = input * 0.5
+    wants = input * 0.3
+    savings = input * 0.2
 
+    result.update ({"Needs": needs, "Wants": wants, "Savings": savings,})
     return result # result should be a dictionary wiht the correct values
